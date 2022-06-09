9f857ff9f187c323846297c1662b750b42e20f81
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-09 14:32:11 +0200

    Show coordinates on cube

---
+++ Assets/Scripts/CoordinateLabel.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+[ExecuteAlways]  //bedzie dzialal w trybie gry i edycji
+public class CoordinateLabel : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+
+            DisplayCoordinates();
+
+        }
+    }
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z);
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+
+
+
+
+}
