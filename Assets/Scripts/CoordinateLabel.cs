19ae861bb6d85f8a4ac8db9774c6accc1f955e84
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-09 14:44:21 +0200

    coordinates are more clearly

--- Assets/Scripts/CoordinateLabel.cs
+++ Assets/Scripts/CoordinateLabel.cs
@@ -27,8 +27,8 @@ public class CoordinateLabel : MonoBehaviour
     }
     void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z);
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
         label.text = coordinates.x + "," + coordinates.y;
     }
 
