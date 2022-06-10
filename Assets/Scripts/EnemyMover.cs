27a1780ac991cf4d112326b82a42cb2448df7a48
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-10 10:28:00 +0200

    created list for waypoints

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintWapointName();
+    }
+    void PrintWapointName()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+        }
+    }
+
+}
