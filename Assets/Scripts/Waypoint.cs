2329f0c5a5976611a4bae75e226cfb18c6f597c0
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-13 20:00:12 +0200

    preparing to building turrets

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isPlaceable;
+    void OnMouseDown()
     {
-        
+        Debug.Log(transform.name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
