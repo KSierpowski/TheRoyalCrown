54e1ab50809f526491804f5ddec757ee9f8238ce
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-15 15:11:40 +0200

    changed "isPlaceable"  bool into public bool

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] GameObject towerPrefab;
+
+
     [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
+
     void OnMouseDown()
     {
         if (isPlaceable)
