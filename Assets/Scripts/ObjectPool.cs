2ca7c7bbc5d97e3fb7b74f2a1655a9efaf5a5e96
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-16 11:47:09 +0200

    Added object pooling

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class ObjectPool : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int poolSize = 5;
     [SerializeField] float spawnTime = 1f;
+
+    GameObject[] pool;
+
+    private void Awake()
+    {
+        PopulatePool();
+    }
+
     void Start()
     {
         StartCoroutine(SpawnEnemy());
     }
+
+
+    void PopulatePool()
+    {
+        pool = new GameObject[poolSize];
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            pool[i] = Instantiate(enemyPrefab, transform);
+            pool[i].SetActive(false);
+        }
+      
+    }
+
+
     IEnumerator SpawnEnemy()
     {
         while (true)
         {
-            Instantiate(enemyPrefab, transform);
+          
             yield return new WaitForSeconds(spawnTime);
         }
 
