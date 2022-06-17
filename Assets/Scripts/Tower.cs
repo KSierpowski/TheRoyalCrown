650b505cbf58c7560fa66b89e21cf840ec0ea2a6
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-17 13:49:21 +0200

    Updated he towers crafting method

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+ public bool CreateTower(Tower tower, Vector3 position)
+
+    {
+        Instantiate(tower.gameObject, position, Quaternion.identity);
+        return true;
+    }
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField] GameObject towerPrefab;
+    [SerializeField] Tower towerPrefab;
 
 
     [SerializeField] bool isPlaceable;
@@ -14,8 +14,8 @@ public class Waypoint : MonoBehaviour
     {
         if (isPlaceable)
         {
-            Instantiate(towerPrefab, transform.position, Quaternion.identity);   
-            isPlaceable = false;
+           bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position); 
+            isPlaceable = !isPlaced;
         }
     }
 
