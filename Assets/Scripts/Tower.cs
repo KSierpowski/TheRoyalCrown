5ec83278eddf18c786264ea424abbfba6e1e72b5
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-17 14:03:02 +0200

    Added turrets cost

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField] int cost = 75;
  public bool CreateTower(Tower tower, Vector3 position)
 
     {
-        Instantiate(tower.gameObject, position, Quaternion.identity);
-        return true;
+        Bank bank = FindObjectOfType<Bank>();
+        if (bank == null)
+        {
+            return false;
+        }
+
+        if (bank.CurrentBalance >= cost)
+        {
+            Instantiate(tower, position, Quaternion.identity);
+            bank.Withdraw(cost);
+            return true;
+        }
+
+        return false;
     }
+
 }
