f91c1cd6e7f51712154c6fb8d11871217f13793b
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-17 14:03:45 +0200

    If the player has lost all their money, refresh the game

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
@@ -23,10 +24,19 @@ public class Bank : MonoBehaviour
 public void Withdraw (int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+
+        if (currentBalance < 0)
+        {
+            ReloadScene();
+        }
     }
 
 
-
+    void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
 
 
 
