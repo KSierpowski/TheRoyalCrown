584e2c26b6b97da9ac503d30aa53af5ec91a3563
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-17 10:55:03 +0200

    The turrets find the closest enemy

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,18 +6,33 @@ public class TargetLocator : MonoBehaviour
 {
     Transform target;
     [SerializeField] Transform weapon;
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
+
 
 
     void Update()
     {
+        FindNearestTarget();
         AimWeapon();
+        
     }
 
+    void FindNearestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform nearestTarget = null;   //null bo jeszcze go nie znaleüliúmy
+        float maxDistance = Mathf.Infinity;   //najwieksza liczba
 
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance)
+            {
+                nearestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = nearestTarget;
+    }
     void AimWeapon()
     {
         weapon.LookAt(target);
