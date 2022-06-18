641256795b7f5ababd29a386362bc0063723c9ca
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-18 10:45:08 +0200

    Added gold balance on the screen

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -11,24 +12,33 @@ public class Bank : MonoBehaviour
 
     public int CurrentBalance { get { return currentBalance; } }  //uzyskujemy dostep do currentbalance ale nie mozemy go edytowac
 
+    [SerializeField] TextMeshProUGUI displayBalance;
 
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
     public void Deposit (int amount)
     {
-        currentBalance += Mathf.Abs(amount); 
+        currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
 public void Withdraw (int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
 
